8030c207b964a98651a66975e63ccb5d4948ec66
Kassia Leão <kassiadarcie@gmail.com>
2025-04-03 22:39:07 -0300

    Atualiza configuração do projeto e implementa geração de dados de sensores aleatórios

--- src/main/java/com/pi_v_b/sistema_de_monitoramento_de_ambientes/RandomSensorData.java
+++ src/main/java/com/pi_v_b/sistema_de_monitoramento_de_ambientes/RandomSensorData.java
@@ -1,7 +1,15 @@
 package com.pi_v_b.sistema_de_monitoramento_de_ambientes;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+/**
+ * Classe responsável por gerar dados aleatórios para simular os dados dos sensores.
+ */
 public class RandomSensorData {
-    public String generateRandomData() {
+    public static String generateRandomData() {
+        Locale.setDefault(Locale.US); // Define o locale para garantir a formatação correta dos números
         float umidade, temperatura, luminosidade;
         umidade = (float) Math.random() * 100;
         temperatura = (float) Math.random() * 100;
